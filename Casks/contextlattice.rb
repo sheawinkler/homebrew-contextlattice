08661be730d1424d1ca18a28822e0146f83f0e43
Shea Winkler <shea.winkler@algotrade-ai.com>
2026-06-12 20:15:22 -0600

    fix(cask): install ContextLattice app bundles

--- Casks/contextlattice.rb
+++ Casks/contextlattice.rb
@@ -1,6 +1,6 @@
 cask "contextlattice" do
-  version "3.4.21"
-  sha256 "8354ad48402e241160a2f1d46c2f565cca01e7c022be8bd715e59161b41c3f6b"
+  version "3.4.23"
+  sha256 "0cac4954725bce0a2aecf64054cc9e096337ec69ab17b4be0769324dcae2b8eb"
 
   url "https://github.com/sheawinkler/ContextLattice/releases/download/v#{version}/ContextLattice-macOS-universal.dmg",
       verified: "github.com/sheawinkler/ContextLattice/"
@@ -15,9 +15,8 @@ cask "contextlattice" do
 
   depends_on macos: :ventura
 
-  artifact "ContextLattice.command", target: "#{appdir}/ContextLattice.command"
-  artifact "Monitoring.command", target: "#{appdir}/ContextLattice Monitoring.command"
-  artifact "README.txt", target: "#{appdir}/ContextLattice README.txt"
+  app "ContextLattice.app"
+  app "ContextLattice Monitoring.app"
 
   zap trash: [
     "~/.contextlattice",
