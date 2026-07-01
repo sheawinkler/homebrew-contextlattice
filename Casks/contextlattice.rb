09beafbab7bfdfcd48b74bc23b6b2cedb825a66f
Shea Winkler <shea.winkler@algotrade-ai.com>
2026-07-01 02:22:27 -0600

    Bump ContextLattice cask to v3.7.1

--- Casks/contextlattice.rb
+++ Casks/contextlattice.rb
@@ -1,6 +1,6 @@
 cask "contextlattice" do
-  version "3.7.0"
-  sha256 "458aa2a1df3d76601f3102ebd2d0e164eab1b15923655b3403cd913b544fc4a5"
+  version "3.7.1"
+  sha256 "55d29a9c2027979f45ecbb5276c110ea41ddb28bd0b20f23497932aad7bc451c"
 
   url "https://github.com/sheawinkler/ContextLattice/releases/download/v#{version}/ContextLattice-macOS-universal.dmg",
       verified: "github.com/sheawinkler/ContextLattice/"
